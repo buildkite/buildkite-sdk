a9381e031fafef71aa4890333dd55d7178c618bd
Zack Chase <zachary@buildkite.com>
2024-10-17 09:49:53 -0700

    Update upload script

--- .buildkite/upload.sh
+++ .buildkite/upload.sh
@@ -6,5 +6,5 @@ echo "--- :pipeline: generating pipeline"
 cd .buildkite
 go run main.go
 
-echo "--- :pipeline_upload: uploading pipeline.json"
-buildkite-agent pipeline upload pipeline.json
+# echo "--- :pipeline_upload: uploading pipeline.json"
+# buildkite-agent pipeline upload pipeline.json
