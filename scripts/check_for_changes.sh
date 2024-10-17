0cdebf88524deb351ccc87f4e3cc8a09abad940a
Zack Chase <zachary@buildkite.com>
2024-10-17 11:31:11 -0700

    Show what changes are present

--- scripts/check_for_changes.sh
+++ scripts/check_for_changes.sh
@@ -5,6 +5,7 @@ set -eo pipefail
 git ls-files --others --error-unmatch . >/dev/null 2>&1; ec=$?
 if test "$ec" = 0; then
     echo "there are changes"
+    git diff
     exit 1
 elif test "$ec" = 1; then
     exit 0
