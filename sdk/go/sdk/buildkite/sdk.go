3afe02f32c65317ff3c16abebd10b17105db22a4
Zack Chase <zachary@buildkite.com>
2025-03-05 12:29:15 -0800

    Add more step types to go

--- sdk/go/sdk/buildkite/sdk.go
+++ sdk/go/sdk/buildkite/sdk.go
@@ -6,12 +6,49 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+type PipelineStep interface {
+	MarshalJSON() ([]byte, error)
+	UnmarshalJSON(data []byte) error
+}
+
 type Pipeline struct {
-	Steps []CommandStep `json:"steps" yaml:"steps"`
+	Steps []PipelineStep `json:"steps" yaml:"steps"`
 }
 
 func (p *Pipeline) AddCommandStep(step CommandStep) {
-	p.Steps = append(p.Steps, step)
+	p.Steps = append(p.Steps, &CommandUnion{
+		CommandStep: &step,
+	})
+}
+
+func (p *Pipeline) AddWaitStep(step WaitStep) {
+	p.Steps = append(p.Steps, &Label{
+		WaitStep: &step,
+	})
+}
+
+func (p *Pipeline) AddInputStep(step InputStep) {
+	p.Steps = append(p.Steps, &Input{
+		InputStep: &step,
+	})
+}
+
+func (p *Pipeline) AddTriggerStep(step TriggerStep) {
+	p.Steps = append(p.Steps, &Trigger{
+		TriggerStep: &step,
+	})
+}
+
+func (p *Pipeline) AddBlockStep(step BlockStep) {
+	p.Steps = append(p.Steps, &Block{
+		BlockStep: &step,
+	})
+}
+
+func (p *Pipeline) AddGroupStep(step GroupStepClass) {
+	p.Steps = append(p.Steps, &SchemaStep{
+		GroupStepClass: &step,
+	})
 }
 
 func (p *Pipeline) ToJSON() (string, error) {
