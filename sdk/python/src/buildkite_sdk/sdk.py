70f328155d56392fc09804bc6119d64422e5425d
Zack Chase <zachary@buildkite.com>
2025-03-05 12:29:28 -0800

    Add more step types to python

--- sdk/python/src/buildkite_sdk/sdk.py
+++ sdk/python/src/buildkite_sdk/sdk.py
@@ -1,4 +1,4 @@
-from buildkite_sdk.schema import CommandStep
+from buildkite_sdk.schema import CommandStep, WaitStep, BlockStep, GroupStepClass, TriggerStep, InputStep
 from buildkite_sdk.environment import Environment
 import json
 import yaml
@@ -20,6 +20,26 @@ class Pipeline:
 
     def add_command_step(self, props: CommandStep):
         """Add a command step to the pipeline."""
+        self.steps.append(props)
+
+    def add_wait_step(self, props: WaitStep):
+        """Add a wait step to the pipeline"""
+        self.steps.append(props)
+
+    def add_block_step(self, props: BlockStep):
+        """Add a block step to the pipeline"""
+        self.steps.append(props)
+
+    def add_input_step(self, props: InputStep):
+        """Add a input step to the pipeline"""
+        self.steps.append(props)
+
+    def add_trigger_step(self, props: TriggerStep):
+        """Add a trigger step to the pipeline"""
+        self.steps.append(props)
+
+    def add_group_step(self, props: GroupStepClass):
+        """Add a group step to the pipeline"""
         self.steps.append(props)
 
     def to_json(self):
