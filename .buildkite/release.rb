5da7d6b2120728f9c33630a076cee3851c47ee60
Zack Chase <zachary@buildkite.com>
2025-03-17 14:03:40 -0700

    Fix up pipeline

--- .buildkite/release.rb
+++ .buildkite/release.rb
@@ -4,16 +4,25 @@ require_relative("../sdk/ruby/lib/environment")
 pipeline = Buildkite::Pipeline.new
 
 plugins = [
-  { "docker#v5.11.0": { image: "buildkite-sdk-tools:latest" } },
+  { "docker#v5.11.0": {
+     image: "buildkite-sdk-tools:latest",
+     "propagate-environment": true,
+     environment: [
+       "GITHUB_TOKEN",
+       "NPM_TOKEN",
+       "PYPI_TOKEN",
+       "GEM_HOST_API_KEY"
+     ]
+  }},
   { "rubygems-oidc#v0.2.0": { role: "rg_oidc_akr_emf87k6zphtb7x7adyrk" } },
   { "aws-assume-role-with-web-identity#v1.0.0": {
     "role-arn": "arn:aws:iam::597088016345:role/marketing-website-production-pipeline-role"
   }},
   { "aws-ssm#v1.0.0": {
     parameters: {
-      NPM_TOKEN: "prod/buildkite-sdk/npm-token",
-      PYPI_TOKEN: "prod/buildkite-sdk/pypi-token",
-      GITHUB_TOKEN: "prod/buildkite-sdk/github-token"
+      NPM_TOKEN: "/prod/buildkite-sdk/npm-token",
+      PYPI_TOKEN: "/prod/buildkite-sdk/pypi-token",
+      GITHUB_TOKEN: "/prod/buildkite-sdk/github-token"
     }
   }}
 ]
