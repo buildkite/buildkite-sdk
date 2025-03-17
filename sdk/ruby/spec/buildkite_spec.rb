8bc75dfb48242631ec1f64548f28ddc3db65fb5e
Zack Chase <zachary@buildkite.com>
2025-03-17 11:04:00 -0700

    Fill out the ruby top level pipeline fields (#31)
    * Fill out the ruby top level pipeline fields
    
    * Address feedback

--- sdk/ruby/spec/buildkite_spec.rb
+++ sdk/ruby/spec/buildkite_spec.rb
@@ -3,12 +3,28 @@ RSpec.describe Buildkite do
     expect(Buildkite::VERSION).not_to be_nil
   end
 
-  it "does something useful" do
+  it "builds a simple pipeline" do
     pipeline = Buildkite::Pipeline.new
 
     pipeline.add_step(label: "some-label", command: "echo 'Hello, World!'")
 
     json = { steps: [{ label: "some-label", "command": "echo 'Hello, World!'" }] }
+    expected = JSON.pretty_generate(json, indent: "    ")
+    expect(pipeline.to_json).to eq(expected)
+  end
+
+  it "builds a pipeline" do
+    pipeline = Buildkite::Pipeline.new
+
+    pipeline.add_agent("queue", "hosted")
+    pipeline.add_environment_variable("FOO", "bar")
+    pipeline.add_step(label: "some-label", command: "echo 'Hello, World!'")
+
+    json = {
+      steps: [{ label: "some-label", "command": "echo 'Hello, World!'" }],
+      agents: { "queue": "hosted" },
+      env: { "FOO": "bar" }
+    }
     expected = JSON.pretty_generate(json, indent: "    ")
     expect(pipeline.to_json).to eq(expected)
   end
