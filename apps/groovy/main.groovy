6e3a1e11e332e5678352f1463866d493ac2c0f58
Christian Nunciato <chris@nunciato.org>
2025-04-05 19:11:12 -0700

    Try runnning a Groovy-generated step

--- apps/groovy/main.groovy
+++ apps/groovy/main.groovy
@@ -5,7 +5,11 @@ def pipeline = new Pipeline()
 
 pipeline.addStep([
     label: "some-label",
-    command: "echo 'Hello, world!'"
+    command: "echo 'Hello, world!'",
 ])
 
-print pipeline.toJSON()
+def outputDir = new File("../../out/apps/groovy")
+outputDir.mkdirs()
+
+new File(outputDir, "pipeline.json").text = pipeline.toJSON()
+new File(outputDir, "pipeline.yaml").text = pipeline.toYAML()
