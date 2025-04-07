ab5f0f0a06617af9eff28993fc521518c30f6180
Zack Chase <zachary@buildkite.com>
2025-04-07 14:52:39 -0700

    Clean up the typescript generated types (#48)
    * Breakout block step
    
    * Breakout command step
    
    * Breakout input step
    
    * Breakout trigger step
    
    * Finish breaking out steps
    
    * Remove generated schema file

---
+++ sdk/typescript/src/inputStep.ts
@@ -0,0 +1,16 @@
+import { DependsOn, Field } from './types'
+
+export interface InputStep {
+    input: string;
+    fields: Field[];
+    allow_dependency_failure?: boolean;
+    branches?: string[] | string;
+    depends_on?: Array<DependsOn | string> | null | string;
+    id?: string;
+    identifier?: string;
+    if?: string;
+    key?: string;
+    label?: string;
+    name?: string;
+    prompt?: string;
+}
