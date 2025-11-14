075e75f0207557a207609014c438f65dbfe189b7
Zack Chase <zachary@buildkite.com>
2025-11-13 18:24:25 -0800

    Add in gen methods [go]

---
+++ internal/gen/go/type.go
@@ -0,0 +1,35 @@
+package gogen
+
+import (
+	"fmt"
+
+	"github.com/buildkite/buildkite-sdk/internal/gen/utils"
+)
+
+type goType struct {
+	Name        string
+	Description string
+	Value       string
+}
+
+func (g goType) String() string {
+	block := utils.NewCodeBlock()
+
+	if g.Description != "" {
+		block.AddLines(NewGoComment(g.Description))
+	}
+
+	block.AddLines(
+		fmt.Sprintf("export type %s = %s", g.Name, g.Value),
+	)
+
+	return block.String()
+}
+
+func NewType(name, description, value string) goType {
+	return goType{
+		Name:        name,
+		Description: description,
+		Value:       value,
+	}
+}
